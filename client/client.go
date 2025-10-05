42189962ec4e8134a11f5271d97f73f10df9f03e
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-10-05 22:23:52 +0530

    client changes also done

--- client/client.go
+++ client/client.go
@@ -1,28 +1,31 @@
 package main
+
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "os"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
 )
+
 func main() {
-    conn, err := net.Dial("tcp", "127.0.0.1:9000")
-    if err != nil {
-        fmt.Println("Error connecting to server:", err)
-        return
-    }
-    defer conn.Close()
-    fmt.Println("Connected to server. Type messages:")
-    go func() {
-        scanner := bufio.NewScanner(conn)
-        for scanner.Scan() {
-            fmt.Println("Server:", scanner.Text())
-        }
-        fmt.Println("Disconnected from server.")
-        os.Exit(0)
-    }()
-    stdin := bufio.NewScanner(os.Stdin)
-    for stdin.Scan() {
-        fmt.Fprintln(conn, stdin.Text())
-    }
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	go func() {
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	consoleReader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		text, _ := consoleReader.ReadString('\n')
+		conn.Write([]byte(text))
+	}
 }
---
+++ server/utils.go
@@ -0,0 +1,7 @@
+package main
+
+import "math/rand"
+
+func RandomInt(max int) int {
+	return rand.Intn(max)
+}
